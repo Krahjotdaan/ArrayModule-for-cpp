8437240d19d1bef820938edd7ba57fd33957091f
Ostapenko <abc@mail.ru>
2023-05-26 14:04:14 +0300

    Feat: added header file

---
+++ arraymodule.h
@@ -0,0 +1,34 @@
+void fill(int[], int, int);
+
+void read(int[], int);
+void read(int[], int, ifstream&);
+
+void print(int[], int);
+void print(int[], int, ofstream&);
+
+int sum(int[], int);
+
+int max(int[], int);
+int min(int[], int);
+
+void reverse(int[], int);
+
+int find(int[], int, int);
+
+void copy(int[], int, int[]);
+
+void insert(int[], int&, int, int);
+
+void erase(int[], int&, int);
+
+int count(int[], int, int);
+
+int lenght(int[]);
+
+bool is_equal(int a[], int size_a, int b[], int size_b);
+
+void rotate(int[], int, int);
+
+void sort(int[], int);
+
+void append(int[], int&, int);
